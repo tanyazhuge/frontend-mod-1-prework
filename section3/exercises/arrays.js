42a85acd94a42e04d0e69552e92428fe76bb654d
Tanya Zhuge <tanyaxz15@gmail.com>
2021-08-04 13:31:08 -0700

    Add Section 3 work

--- section3/exercises/arrays.js
+++ section3/exercises/arrays.js
@@ -24,16 +24,20 @@ console.log(animals[0]);
 
 // YOU DO: Write code below that will log the number of elements in array of
 // animals from above.
+console.log(animals.length);
 
 
 // YOU DO: Write code that will reassign the last item in the animals
 // array to "Gorilla"
+animals[-1] = "Gorilla";
 
 
 // YOU DO: Write code that will add a new animal (type of your choice) to position 3.
 
+animals.splice(2,0,"tiger");
 
 // YOU DO: Write code that will log the String "Elephant" in the animals array
+console.log(animals[3]); //Since tiger has replaced elephant in position 3, elephant index is at 3.
 
 
 //-------------------
@@ -41,26 +45,35 @@ console.log(animals[0]);
 //-------------------
 
 // YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
+var foods = ["egg", "salmon", "chicken", "lettuce"]
 
 
 // YOU DO: Write code below that will log the number of elements in the array of
 // foods from above.
-
+console.log(foods.length);
 
 // YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 // log the changed array to verify "broccoli" has been added
-
+foods.unshift("broccoli");
+console.log(foods);
 
 // YOU DO: Write code below that removes the last item of food from the foods array and
 // log the changed array to verify that item has been removed
+foods.pop();
+console.log(foods);
 
 
-// YOU DO: Write code to add 3 new foods to the array. 
+// YOU DO: Write code to add 3 new foods to the array.
   // There are several ways to do this - choose whichever you'd like!
 // Then, log the changed array to verify the new items have been added
+foods.unshift("tomato") //insert beginning of array
+foods.push("cabbage") //insert at end of array
+foods.push("carrot")
+
+console.log(foods);
 
 // YOU DO: Remove the food that is in index position 0.
-
+foods.shift()
 //-------------------
 // PART 3: Where are Arrays used?
 //-------------------
@@ -79,12 +92,13 @@ The post itself likely has more complex data, but here's one way we can think ab
 var posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 // YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-// may be storing data? Come up with 3 examples - they could be from different web applications or 
+// may be storing data? Come up with 3 examples - they could be from different web applications or
 // all from the same one.
 
-// 1:
-// 2:
-// 3:
+// 1: Grocery App: saving the list of items that I have searched before. When I start a new search, the list of items previously
+//searched are displayed.
+// 2: Purchasing tickets online: filling out name, it showed a list of prefix to select from.
+// 3: Purchasing ticket sonline: filling out the address section, displayed list of states to select from.
 
 
 //-------------------
@@ -96,7 +110,7 @@ YOU DO:
 Using the variables defined below, write a program that will tell a user if they
 will be able to call an Uber.
 
-The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter 
+The user can call an uber if they have 15% battery remaining, or more. In this case, it doesn't matter
 if the user has a charger at all, or what type.
 The can call an uber if they have a charger and it is a car charger.
 
@@ -106,6 +120,25 @@ var hasCharger = true;
 var chargerType = "car";
 
 // Write your conditional here
+
+//Create a function called callUber with 3 parameters(percentBatteryLeft, hasCharger, chargerType)
+function callUber(percentBatteryLeft, hasCharger, chargerType) {
+  // If statement checks condition if percentBatteryLeft is larger or equal to 15 by using comparison operator
+if (percentBatteryLeft >=15) {
+  // If above statement is true, then prints the string "Phone can call an Uber"
+  console.log("Phone can call Uber");
+  // else if statement checks condition of must having a car charger by using logical operator
+} else if (hasCharger === true && chargerType === "car" ){
+  // If above statement is true, then prints the string "Phone can call an Uber"
+  console.log("Phone can call Uber");
+  // else statement for rest of scenarios not in the first two conditions
+} else {
+  // If above statement is true, then prints the string "Not able to call an Uber"
+  console.log("Not able to call an Uber!");
+}
+  }
+// Calling the callUber function and running it
+callUber(percentBatteryLeft, hasCharger, chargerType)
 
 
 // YOU DO - ANNOTATE: Above each line of your code for this if statement, create a comment.
--- section3/exercises/loops.js
+++ section3/exercises/loops.js
@@ -25,20 +25,36 @@ for (var i = 0; i < 3; i++) {
 // What is logged to the console for this second example? Why?
 // EXPLAIN:
 
+/*The number 0,1,2 are logged to the console for the 2nd example.
+If the condition i is less than 3, execute loop. Zero is less than 3, it prints 0 for and adds 1 to i. One is less than 3, it prints 1 for
+and adds 1 to i. Two is less than 3, it prints 2 and add 1 to i. Three is not less than 3, condition is not met. The loops ends.
+*/
 
  // YOU DO: Write code below that logs to the console, the sum of 2 plus 2, 7 times:
+ for (var i = 0; i < 7; i++){
+   console.log( 2 + 2 );
+ }
 
 
  // YOU DO: Write code below that logs the String 'She sells seashells down by the seashore'
  // 10 times.
+
+ for (var i = 0; i < 10; i++){
+   console.log("She sells seashells down by the seashore");
+ }
 
 
 //-------------------
 // LEVEL 2: An Added Layer of Challenge
 //-------------------
 
-// YOU DO: Write code below that logs to the console a String of "This is loop number: " 
+// YOU DO: Write code below that logs to the console a String of "This is loop number: "
 //that concatenates with `i`.
+
+for (var i = 1; i < 6; i++){
+// console.log i not "i", since i is variable and can change "i" is string and would not change
+  console.log("This is loop number:" + i);
+}
 
 // YOU DO: Using a for loop, how could you get an output that looks like this:
 
@@ -53,6 +69,11 @@ for (var i = 0; i < 3; i++) {
 // 2
 // 1
 
-// Not sure? Try to google some terms that seem relevant. Play around with it. Don't spend 
+// starts with 10 and decrease i
+for (var i = 10; i >= 1; i--){
+  console.log(i);
+}
+
+// Not sure? Try to google some terms that seem relevant. Play around with it. Don't spend
 // more than 15 minutes without making progress. If you are at 15 minutes and haven't made
 // progress, that means it's time to reach out to your pre-work group on Slack!
